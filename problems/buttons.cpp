c346f824b95e2d18e2f6e4a14e206d2c51fa28b3
Kevin Gilbert <pompeii@vulcan.(none)>
2015-09-29 02:33:38 -0500

    Rough implementation completed

--- problems/buttons.cpp
+++ problems/buttons.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <iterator>
 
 using namespace std;
 
 void generate_ladder(vector<int>& ladder, int goal) {
+    ladder.push_back(goal);
     while(goal != 1) {
         if(goal & 0x0001)   // last bit set, odd
             goal += 1;
@@ -13,6 +16,8 @@ void generate_ladder(vector<int>& ladder, int goal) {
     }
 }
 
+
+//#define DEBUG
 int main(int argc, char **argv) {
     int start = atoi(argv[1]);
     int goal  = atoi(argv[2]);
@@ -26,4 +31,34 @@ int main(int argc, char **argv) {
         cout << i << " ";
     cout << endl;
     #endif
+
+    int ladder_start = 0;
+    for(auto i : ladder) {
+        static int prev_i;
+        #ifdef DEBUG
+        cout << i << endl;
+        #endif
+        if(i == start) {
+            ladder_start = i;
+            break;
+        }
+        if(i < start) {
+            ladder_start = i;
+            break;
+        }
+        prev_i = i;
+    }
+    
+    steps = start - ladder_start;
+    while(start != ladder_start)
+        --start;
+   
+    vector<int>::iterator lstart_iter = find(ladder.begin(), ladder.end(), ladder_start);
+    #ifdef DEBUG
+    cout << "Steps: " << steps << endl;
+    cout << "Ladder Iterator: " << *lstart_iter << endl;
+    cout << "Ladder start: " << distance(ladder.begin(), lstart_iter) << endl;     
+    #endif
+    cout << steps + distance(ladder.begin(), lstart_iter) << endl;
+    return 0;
 }
